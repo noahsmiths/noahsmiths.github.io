1871fdd934547cced0ed61f1aba8d0412c8c7558
noahsmiths <90880131+noahsmiths@users.noreply.github.com>
2021-11-02 16:13:18 -0400

    made video player play on button press

---
+++ lol/script.js
@@ -0,0 +1,21 @@
+let player;
+
+const loadVideo = () => {
+    player = new YT.Player('player', {
+        height: '390',
+        width: '640',
+        videoId: 'dQw4w9WgXcQ',
+        playerVars: {
+            'playsinline': 1
+        },
+        events: {
+            'onReady': (e) => {
+                e.target.playVideo();
+            },
+        }
+    });
+    document.getElementById("initial-content").style.display = "none";
+    document.getElementById("vid-container").style.display = "initial";
+}
+
+document.getElementById("load-button").addEventListener("click", loadVideo);
