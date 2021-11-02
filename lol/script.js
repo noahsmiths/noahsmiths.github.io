ecc8809b064d929c1c2d11feae2de760574160c2
noahsmiths <90880131+noahsmiths@users.noreply.github.com>
2021-11-02 16:33:36 -0400

    Update script.js

--- lol/script.js
+++ lol/script.js
@@ -23,24 +23,4 @@ const loadVideo = () => {
     });
 }
 
-window.onload = () => {
-    if (localStorage.getItem("loadedPreviously")) {
-        player = new YT.Player('player', {
-            height: screen.availHeight * 0.7,
-            width: screen.availWidth * 0.95,
-            videoId: 'dQw4w9WgXcQ',
-            playerVars: {
-                'playsinline': 1
-            },
-            events: {
-                'onReady': (e) => {
-                    e.target.playVideo();
-                    document.getElementById("initial-content").style.display = "none";
-                    document.getElementById("vid-container").style.display = "initial";
-                },
-            }
-        });
-    }
-}
-
 document.getElementById("load-button").addEventListener("click", loadVideo);
